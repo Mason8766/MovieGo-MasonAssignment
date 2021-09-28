3c6c7704edaa7c36c48586a83eb8a7874e07bd83
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2021-09-28 15:23:28 -0400

    Added pages(views), and movie controller. Edited nav bar. edited title.

---
+++ MovieGo-MasonAssignment/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieGo_MasonAssignment.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+        public IActionResult MovieReview()
+        {
+            return View();
+        }
+    }
+}
