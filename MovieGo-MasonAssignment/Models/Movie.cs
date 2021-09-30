b37448ce6e761d4332ccdc1debc27a8ca2eb8cbb
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2021-09-30 08:52:54 -0400

    Added Models

---
+++ MovieGo-MasonAssignment/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieGo_MasonAssignment.Models
+{
+    public class Movie
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        public string Year { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        public string[] Genre { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        [Range(0,10, ErrorMessage = "Movies can only be rated from 0-10")]
+        public double Rating { get; set; }
+    }
+}
---
+++ MovieGo-MasonAssignment/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieGo_MasonAssignment.Models
+{
+    public class Review
+    {
+        Movie Movie { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        public string ReviewPara { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Field required")]
+        [Range(0, 10, ErrorMessage = "Movies can only be rated from 0-10")]
+        public double UserRating { get; set; }
+    }
+}
