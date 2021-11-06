54c83ba68e07d7e4da82e8e5d75f249c656b4db4
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2021-11-06 11:15:52 -0400

    Account conformation disabled

--- MovieGo-MasonAssignment/Startup.cs
+++ MovieGo-MasonAssignment/Startup.cs
@@ -32,7 +32,7 @@ namespace MovieGo_MasonAssignment
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
